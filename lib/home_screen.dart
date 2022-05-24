55fa78362167705e75cbdce3fe3ddd321e55246f
Manuel-Rivera <1567079x@umich.mx>
2022-05-23 19:32:45 -0500

    7:32-23/05/2022

---
+++ lib/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //leading:const Icon(Icons.account_circle_rounded),
+        title: const Text("Home TTicket"),
+      ),drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
+      body: const Center(
+          child: Text("HomeScreen")
+        )
+    );
+  }
+}
